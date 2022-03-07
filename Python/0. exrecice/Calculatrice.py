396f1792f58cd38d2299bf99f1a706812f1124b0
Damien Martin <martindamien284@gmail.com>
2022-03-07 08:00:32 +0100

    ajout fichier

---
+++ Python/0. exrecice/Calculatrice.py
@@ -0,0 +1,4 @@
+nombre_1 = input("Veuillez entrer un premier nombre :")
+nombre_2 = input("Veuillez entrer un deuxième nombre :")
+reponse = f"Le résultat de l'addition du nombre {nombre_1} avec le nombre {nombre_2} est égal à {int(nombre_1)+int(nombre_2)}"
+print(reponse)
